a772d1f7813676e79c42c4f712b64741ccd32438
sid-darth-73 <siddharthraj532@gmail.com>
2025-12-11 11:43:47 +0530

    Revert "add deploy, export scripts in package json"
    This reverts commit b46691b58aeae11c188e29738d1c9cf8427b3fbf.

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  basePath: '/cubey-v2',
-  assetPrefix: '/cubey-v2/'
 };
 
 export default nextConfig;
